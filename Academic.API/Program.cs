4d7c801db9f934a20ce14c549c5041534b60279a
joseucarvajal <joseucarvajal@gmail.com>
2020-10-04 22:01:45 -0500

    Data seeding (courseGroup) entity

--- Academic.API/Program.cs
+++ Academic.API/Program.cs
@@ -1,9 +1,13 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Academic.Infrastructure;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -13,14 +17,41 @@ namespace Academic.API
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateWebHostBuilder(args).Build();
+            SeedDatabase(host);
+            host.Run();
         }
 
+        /*
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+        */
+
+        private static void SeedDatabase(IWebHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<AcademicDbContext>();
+                    new AcademicDbSeed().SeedAsync(context, services).Wait();                    
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError("An error occurred while seeding the database");
+                }
+            }
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+           WebHost.CreateDefaultBuilder(args)
+               .UseStartup<Startup>()
+               .UseContentRoot(Directory.GetCurrentDirectory());
     }
 }
---
+++ Academic.Infrastructure/AcademicDbSeed.cs
@@ -0,0 +1,35 @@
+﻿using Academic.Domain.CourseAllocationAggregate;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Academic.Infrastructure
+{
+    public class AcademicDbSeed
+    {
+        public async Task SeedAsync(AcademicDbContext context, IServiceProvider services)
+        {
+            // Get a logger
+            var logger = services.GetRequiredService<ILogger<AcademicDbSeed>>();
+
+            context.Database.EnsureCreated();
+
+            if (await context.CourseGroups.AnyAsync())
+            {
+                return;
+            }
+
+            context.CourseGroups.Add(CourseGroup.G1);
+            context.CourseGroups.Add(CourseGroup.G2);
+            context.CourseGroups.Add(CourseGroup.G3);
+            context.CourseGroups.Add(CourseGroup.G4);
+
+            await context.SaveChangesAsync();
+        }
+    }
+}
